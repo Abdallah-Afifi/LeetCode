466b32ee5ef14b4fa4526630dd08f45010ff1655
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2024-03-31 23:29:00 +0200

    [860] [Time Beats: 19.76%] [Memory Beats: 66.39%] - LeetPush

---
+++ 860-Lemonade-Change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n5 = 0 , n10 = 0 ;
+        int n= bills.size();
+        for(int  i=0; i<n; i++)
+        {
+            if (bills[i] == 5) ++n5;
+            else if(bills[i] == 10 )
+            {
+                if(n5) {
+                    --n5;
+                    ++n10;
+                }
+                else return false;
+            }
+            else
+            {
+                if(n10 && n5)
+                {
+                    --n10;
+                    --n5;
+                }
+                else
+                {
+                    if(n5>=3)
+                    {
+                        n5-=3;
+                    }
+                    else return false;
+                }
+            }
+        }
+        return true;
+    }
+};
