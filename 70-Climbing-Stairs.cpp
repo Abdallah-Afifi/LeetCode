2d2eb84165de56f349ab4d7a3987a75953393d6c
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2024-03-31 23:21:45 +0200

    [70] [Time Beats: 5.10%] [Memory Beats: 27.94%] - LeetPush

---
+++ 70-Climbing-Stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+map<int, int> memo; 
+    int climbStairs(int n) {
+        // make it efficient
+        auto it = memo.find(n); 
+        if(it != memo.end())
+            return it->second; 
+
+        // make it work 
+        // base case 
+        if(n == 0)
+            return 1; 
+        if(n < 0){
+            memo[n] = 0; 
+            return 0; 
+        }
+        memo[n] = climbStairs(n-1) + climbStairs(n-2); 
+        return memo[n]; 
+    }
+};
